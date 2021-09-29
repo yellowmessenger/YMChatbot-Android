591bd14672bc1494f7082c27f681a470b6d84c46
Sukrit Gupta <sukrit.gupta@yellowmessenger.com>
2021-09-29 13:31:53 +0530

    Initial commit

--- app/src/main/java/com/yellowmessenger/ymchatexample/MainActivity.java
+++ app/src/main/java/com/yellowmessenger/ymchatexample/MainActivity.java
@@ -16,8 +16,8 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Dummy bot id. (Purrs a lot)
-    String botId = "x1587041004122";
+    // Dummy bot id. (Purrs a lot)J
+    String botId = "x1589891354544"; //"x1587041004122";
     String deviceToken = "your device token";
     String apiKey = "your api key";
 
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         //Get YMChat instance
         YMChat ymChat = YMChat.getInstance();
         ymChat.config = new YMConfig(botId);
-        ymChat.config.enableSpeech = true;
+        //ymChat.config.enableSpeech = true;
         //Payload attributes
         HashMap<String, Object> payloadData = new HashMap<>();
         //Setting Payload Data
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         // To Change the color of close button, default color is white
         ymChat.config.closeButtonColor = R.color.white;
 
+        ymChat.config.customBaseUrl = "https://staging.yellowmessenger.com";
         //setting event listener
         ymChat.onEventFromBot((YMBotEventResponse botEvent) -> {
             switch (botEvent.getCode()) {
