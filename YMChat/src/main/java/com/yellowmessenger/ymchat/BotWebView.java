380f88bc1533204b7e12a27a2f6582859d3cc1af
Sukrit Gupta <sukrit.gupta@yellowmessenger.com>
2021-11-23 12:13:59 +0530

    Formatting

--- YMChat/src/main/java/com/yellowmessenger/ymchat/BotWebView.java
+++ YMChat/src/main/java/com/yellowmessenger/ymchat/BotWebView.java
@@ -14,7 +14,6 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
